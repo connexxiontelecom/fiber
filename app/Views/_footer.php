010de27e88662ff052e5bedbc8ddf9927d028e6c
cjamanambu <oamanambu@yahoo.com>
2021-05-11 14:57:04 +0100

    Initial project setup

---
+++ app/Views/_footer.php
@@ -0,0 +1,17 @@
+<?php
+  $session = session();
+?>
+<div class="nk-footer">
+  <div class="container wide-xl">
+    <div class="nk-footer-wrap g-2">
+      <div class="nk-footer-copyright"> &copy; 2020 Fiber Self-Service. Powered by <a href="https://telecom.connexxiongroup.com" target="_blank">Connexxion Telecom</a>
+      </div>
+      <div class="nk-footer-links">
+        <ul class="nav nav-sm">
+          <li class="nav-item"><a class="nav-link" href="#">Terms</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Privacy</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ app/Views/_head.php
@@ -0,0 +1,13 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="author" content="Connexxion Telecom">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Fiber optics service platform for customer self-service">
+  <!-- Fav Icon  -->
+  <link rel="shortcut icon" href="/assets/images/favicon.png">
+  <!-- Page Title  -->
+  <title>Dashboard | Fiber Self-Service</title>
+  <!-- StyleSheets  -->
+  <link rel="stylesheet" href="/assets/css/dashlite.css?ver=2.4.0">
+  <link id="skin-default" rel="stylesheet" href="/assets/css/theme.css?ver=2.4.0">
+</head>
