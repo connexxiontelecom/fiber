810fdc55ae86add6f975939157cf3e49c03410e2
cjamanambu <oamanambu@yahoo.com>
2021-05-24 12:21:07 +0100

    adds manage customer function

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -29,12 +29,14 @@ class User extends BaseController {
   public function manage_customer($user_id) {
     if ($this->session->active) {
       $customer = $this->_get_customer($user_id);
+      $payment_methods = $this->paymentMethodModel->findAll();
       if (!$customer) {
         return $this->_not_found();
       }
       if ($this->session->is_admin) {
         $page_data['title'] = 'Manage Customer';
         $page_data['customer'] = $customer;
+        $page_data['payment_methods'] = $payment_methods;
         return view('user-management/manage-customer', $page_data);
       }
       return $this->_unauthorized();
@@ -108,6 +110,41 @@ class User extends BaseController {
       } else {
         $response_data['success'] = false;
         $response_data['msg'] = 'There was a problem updating the user';
+        $response_data['meta'] = $this->validation->getErrors();
+      }
+      return $this->response->setJSON($response_data);
+    }
+    return redirect('auth');
+  }
+
+  public function manage_customer_info() {
+    if($this->session->active) {
+      $this->validation->setRules([
+        'user_id' => 'required',
+        'payment_method' => 'required',
+        'phone' => 'required',
+        'address' => 'required'
+      ]);
+      $response_data = array();
+      if ($this->validation->withRequest($this->request)->run()) {
+        $post_data = $this->request->getPost();
+        $customer_info = array(
+          'user_id' => $post_data['user_id'],
+          'payment_method_id' => $post_data['payment_method'],
+          'phone' => $post_data['phone'],
+          'address' => $post_data['address']
+        );
+        $customer_info_updated = $this->customerInfoModel->save($customer_info);
+        if ($customer_info_updated) {
+          $response_data['success'] = true;
+          $response_data['msg'] = 'Successfully updated customer info';
+        } else {
+          $response_data['success'] = false;
+          $response_data['msg'] = 'There was a problem updating the customer info';
+        }
+      } else {
+        $response_data['success'] = false;
+        $response_data['msg'] = 'There was a problem updating the customer info';
         $response_data['meta'] = $this->validation->getErrors();
       }
       return $this->response->setJSON($response_data);
