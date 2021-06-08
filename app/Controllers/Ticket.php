226b52e93db34e14428d7d235ccfe0cb58bd4931
cjamanambu <oamanambu@yahoo.com>
2021-06-08 13:07:24 +0100

    adds ability to send ticket response for customer

--- app/Controllers/Ticket.php
+++ app/Controllers/Ticket.php
@@ -22,6 +22,22 @@ class Ticket extends BaseController {
     return redirect('auth');
   }
 
+  public function view_ticket($ticket_id) {
+    if ($this->session->active) {
+      $page_data['title'] = 'View Ticket';
+      $ticket = $this->_get_ticket($ticket_id);
+      if ($ticket) {
+        $page_data['ticket'] = $ticket;
+        if ($this->session->is_admin) {
+          return view('ticket/view-ticket', $page_data);
+        }
+        return view('ticket/view-ticket-alt', $page_data);
+      }
+      return $this->_not_found();
+    }
+    return redirect('auth');
+  }
+
   public function create_ticket() {
     if ($this->session->active) {
       $this->validation->setRules([
@@ -66,5 +82,62 @@ class Ticket extends BaseController {
       return $this->response->setJSON($response_data);
     }
     return redirect('auth');
+  }
+
+  public function create_customer_ticket_response() {
+    if ($this->session->active) {
+      $this->validation->setRules([
+        'ticket_id' => 'required',
+        'body' => 'required',
+      ]);
+      $response_data = array();
+      if ($this->validation->withRequest($this->request)->run()) {
+        $post_data = $this->request->getPost();
+        $ticket_data = array(
+          'sender_id' => $this->session->user_id,
+          'receiver_id' => 1,
+          'ticket_id' => $post_data['ticket_id'],
+          'body' => $post_data['body'],
+        );
+        $ticket_response = $this->ticketResponseModel->insert($ticket_data);
+        if ($ticket_response) {
+          $this->_create_new_notification(
+            $this->session->user_id,
+            1,
+            $ticket_response,
+            'submit_ticket_response',
+            'A new ticket response has been submitted'
+          );
+          $response_data['success'] = true;
+          $response_data['msg'] = 'Successfully submitted a new ticket response';
+        } else {
+          $response_data['success'] = false;
+          $response_data['msg'] = 'There was a problem submitting a new ticket response';
+        }
+      } else {
+        $response_data['success'] = false;
+        $response_data['msg'] = 'There was a problem submitting a new ticket response';
+        $response_data['meta'] = $this->validation->getErrors();
+      }
+      return $this->response->setJSON($response_data);
+    }
+    return redirect('auth');
+  }
+
+  private function _get_ticket($ticket_id) {
+    $ticket = $this->ticketModel->find($ticket_id);
+    if ($ticket) {
+      $customer = $this->userModel->find($ticket['sender_id']);
+      $responses = $this->ticketResponseModel->where('ticket_id', $ticket['ticket_id'])->findAll();
+      foreach ($responses as $key => $res) {
+        $sender = $this->userModel->find($res['sender_id']);
+        $receiver = $this->userModel->find($res['receiver_id']);
+        $responses[$key]['sender'] = $sender;
+        $responses[$key]['receiver'] = $receiver;
+      }
+      $ticket['customer'] = $customer;
+      $ticket['responses'] = $responses;
+    }
+    return $ticket;
   }
 }
---
+++ app/Views/ticket/view-ticket-alt.php
@@ -0,0 +1,149 @@
+<?php
+$session = session();
+?>
+<!DOCTYPE html>
+<html lang="en" class="js">
+<?php include(APPPATH.'/Views/_head.php'); ?>
+<body class="nk-body bg-white npc-default has-aside">
+<div class="nk-app-root">
+  <div class="nk-main">
+    <div class="nk-wrap">
+      <?php include(APPPATH.'/Views/_header.php'); ?>
+      <div class="nk-content">
+        <div class="container wide-xl">
+          <div class="nk-content-inner">
+            <?php include(APPPATH.'/Views/_sidebar.php'); ?>
+            <div class="nk-content-body">
+              <div class="nk-content-wrap">
+                <div class="nk-block-head nk-block-head-lg">
+                  <div class="nk-block-head-sub"><a class="back-to" href="/ticket"><em class="icon ni ni-arrow-left"></em><span>Tickets</span></a></div>
+                  <div class="nk-block-between-md g-4">
+                    <div class="nk-block-head-content">
+                      <h2 class="nk-block-title fw-normal">View Ticket #<?=$ticket['id']?></h2>
+                    </div>
+                    <div class="nk-block-head-content">
+                    </div>
+                  </div>
+                </div><!-- .nk-block-head -->
+                <div class="nk-block">
+                  <div class="ticket-info">
+                    <ul class="ticket-meta">
+                      <li class="ticket-id"><span>Ticket Subject:</span> <strong><?=$ticket['subject']?></strong></li>
+                      <br>
+                      <li class="ticket-date">
+                        <span>Submitted:</span>
+                        <strong>
+                          <?php
+                            $date = date_create($ticket['created_at']);
+                            echo date_format($date, 'd M Y');
+                          ?>
+                        </strong>
+                      </li>
+                    </ul>
+                    <div class="ticket-status">
+                      <?php if ($ticket['status'] == 0):?>
+                        <span class="badge badge-light">Closed</span>
+                      <?php elseif ($ticket['status'] == 1):?>
+                        <span class="badge badge-success">Open</span>
+                      <?php endif;?>
+                    </div>
+                  </div>
+                </div><!-- .nk-block -->
+                <div class="nk-block nk-block-lg">
+                  <div class="card card-bordered">
+                    <div class="card-inner">
+                      <div class="ticket-msgs">
+                        <div class="ticket-msg-item">
+                          <div class="ticket-msg-from">
+                            <div class="ticket-msg-user user-card">
+                              <div class="user-avatar bg-primary">
+                                <span><em class="icon ni ni-user-alt"></em></span>
+                              </div>
+                              <div class="user-info">
+                                <span class="lead-text"><?=$ticket['customer']['name']?></span>
+                                <span class="text-soft">You</span>
+                              </div>
+                            </div>
+                            <div class="ticket-msg-date">
+                              <span class="sub-text">
+                                <?php
+                                  $date = date_create($ticket['created_at']);
+                                  echo date_format($date, 'd M Y H:i a');
+                                ?>
+                              </span>
+                            </div>
+                          </div>
+                          <div class="ticket-msg-comment">
+                            <?=$ticket['body']?>
+                          </div>
+                        </div><!-- .ticket-msg-item -->
+                        <?php if (!empty($ticket['responses'])): foreach ($ticket['responses'] as $res):?>
+                          <div class="ticket-msg-item">
+                            <div class="ticket-msg-from">
+                              <?php if ($res['sender']['user_id'] == $session->user_id):?>
+                                <div class="ticket-msg-user user-card">
+                                  <div class="user-avatar bg-primary">
+                                    <span><em class="icon ni ni-user-alt"></em></span>
+                                  </div>
+                                  <div class="user-info">
+                                    <span class="lead-text"><?=$res['sender']['name']?></span>
+                                    <span class="text-soft">You</span>
+                                  </div>
+                                </div>
+                              <?php else: ?>
+                                <div class="ticket-msg-user user-card">
+                                  <div class="user-avatar bg-warning">
+                                    <span><em class="icon ni ni-user-alt"></em></span>
+                                  </div>
+                                  <div class="user-info">
+                                    <span class="lead-text"><?=$res['sender']['name']?></span>
+                                    <span class="text-soft">Support Team</span>
+                                  </div>
+                                </div>
+                              <?php endif;?>
+                              <div class="ticket-msg-date">
+                                <span class="sub-text">
+                                  <?php
+                                    $date = date_create($res['created_at']);
+                                    echo date_format($date, 'd M Y H:i a');
+                                  ?>
+                                </span>
+                              </div>
+                            </div>
+                            <div class="ticket-msg-comment">
+                              <?=$res['body']?>
+                            </div>
+                          </div><!-- .ticket-msg-item -->
+                        <?php endforeach; endif?>
+                        <div class="ticket-msg-reply">
+                          <h5 class="title">Reply</h5>
+                          <form action="#" id="customer-ticket-reply-form">
+                            <div class="form-group">
+                              <textarea class="form-control" placeholder="Write a message..." name="body" id="body"></textarea>
+                            </div>
+                            <input type="hidden" value="<?=$ticket['ticket_id']?>" id="ticket-id">
+                            <div class="form-action">
+                              <ul class="form-btn-group">
+                                <li class="form-btn-primary"><button class="btn btn-primary">Send</button></li>
+                                <li class="form-btn-secondary"><button type="button" class="btn btn-dim btn-outline-light">Mark as close</button></li>
+                              </ul>
+                            </div>
+                          </form>
+                        </div><!-- .ticket-msg-reply -->
+                      </div><!-- .ticket-msgs -->
+                    </div>
+                  </div>
+                </div><!-- .nk-block -->
+              </div>
+              <?php include(APPPATH.'/Views/_footer.php'); ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include(APPPATH.'/Views/_scripts.php'); ?>
+<?php include('_ticket-scripts.php')?>
+</body>
+</html>
