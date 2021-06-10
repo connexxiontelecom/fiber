fdde4e46425f83294920b314f7e18cd984af12fa
cjamanambu <oamanambu@yahoo.com>
2021-06-10 08:58:30 +0100

    adds cancelled subscription email

--- app/Controllers/Subscription.php
+++ app/Controllers/Subscription.php
@@ -179,8 +179,20 @@ class Subscription extends BaseController {
       );
       $cancel_subscription = $this->subscriptionModel->save($subscription_data);
       if ($cancel_subscription) {
+        $subscription = $this->subscriptionModel->find($subscription_id);
+        $customer = $this->userModel->find($subscription['user_id']);
+        $email_data['data']['name'] = $customer['name'];
+        $email_data['data']['description'] = $subscription['description'];
+        $email_data['data']['end_date'] = date_format(date_create($subscription['end_date']), 'd M Y');
+        $email_data['subject'] = 'Fiber Portal Subscription Cancelled';
+        $email_data['email_body'] = 'cancel-subscription';
+        $email_data['email'] = $customer['email'];
+        $email_data['from_name'] = 'Connexxion Telecom Support';
+        $email_data['from_email'] = 'support@connexxiontelecom.com';
+        $this->send_mail($email_data);
         $response_data['success'] = true;
         $response_data['msg'] = 'Successfully cancelled the subscription';
+        return $this->response->setJSON($response_data);
       } else {
         $response_data['success'] = false;
         $response_data['msg'] = 'There was a problem cancelling the subscription';
